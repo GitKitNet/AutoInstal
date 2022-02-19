12a2199d497f3461f47be02a038240557021ad7d
numbnet <zusyurec@gmail.com>
2022-02-20 01:33:36 +0200

    Update wpca.sh

--- wp/wpca.sh
+++ wp/wpca.sh
@@ -5,6 +5,8 @@
 #==========================================================
 
 
+
+
 #----------------------------------------------------------
 # Colors settings
 #----------------------------------------------------------
@@ -21,22 +23,40 @@ echo -e "Welcome to WordPress & LAMP stack installation and configuration wizard
 First of all, we going to check all required packeges..."
 sleep 5
 
-echo -e -n "${YELLOW}Please enter website name: ${NC}" && read domain
 
-#Checking packages
-echo -e "${YELLOW}Checking packages...${NC}"
-echo -e "List of required packeges: nano, zip, unzip, mc, htop, fail2ban, apache2 & php, mysql, php curl, phpmyadmin, wget, curl"
 
-read -r -p "Do you want to check packeges? [y/N] " response
-case $response in
-		[yY][eE][sS]|[yY]) 
+#================================
+#     
+#================================
+echo -e -n "${YELLOW}Please, provide us with your domain name: ${NC}" && read domain
+echo -e -n "${YELLOW}Please, provide us with your email: ${NC}" && read domain_email
 
-	echo -e "${YELLOW}Upgradin now all Packages ${NC}" && sleep 3;
-	apt-get update --yes && apt-get upgrade --yes;
-	apt-get -y install software-properties-common
-	apt-get update
-	add-apt-repository ppa:ondrej/php -y
-	apt-get update
+dbPassword=$(date +%s|sha256sum|base64|head -c 25) && db_pass="$dbPassword"
+wp_pass=$(date +%s|sha256sum|base64|head -c 20)
+dbNameandUser=$(echo ${domain} | tr "." "_" | tr "-" "_") && db_user="$dbNameandUser";
+authorization="mysql -uroot -p${db_pass}"
+
+wp_admin=root
+wp_pass=$(date +%s|sha256sum|base64|head -c 20)
+Host=\'%\'
+#================================
+
+
+
+
+#-----------------------------------
+#       Checking packages
+#-----------------------------------
+function CHECking_PACK() {
+
+echo -e "${YELLOW}Upgradin now all Packages ${NC}" && sleep 3;
+apt-get update --yes && \
+	apt-get upgrade --yes;
+
+apt-get -y install software-properties-common
+apt-get update
+add-apt-repository ppa:ondrej/php -y
+apt-get update
 
 NANO=$(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed")
 	if [ "$NANO" -eq 0 ]; then
@@ -115,23 +135,29 @@ CURL=$(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed")
 		echo -e "${GREEN}curl	- is installed!${NC}"
 	fi
 
-	;;
+}
 
-		*) echo -e "${RED} Packeges check is ignored! \nPlease be aware, that apache2, mysql,phpmyadmin and other software may not be installed! ${NC}" ;;
+echo -e "${YELLOW}Checking packages...${NC}"
+echo -e "List of required packeges: nano, zip, unzip, mc, htop, fail2ban, apache2 & php, mysql, php curl, phpmyadmin, wget, curl"
+read -r -p "Do you want to check packeges? [y/N] " response
+case $response in
+	[yY][eE][sS]|[yY]) CHECking_PACK ;;
+	*) echo -e "${RED}Packeges check is ignored! \nSoftware may not be installed! ${NC}" ;;
 esac
 
 
-#phpmyadmin default path change
-echo -e "${YELLOW}Changing phpMyAdmin default path from /phpMyAdmin to /myadminphp...${NC}"
 
-read -r -p "Do you want to change default phpMyAdmin path to /myadminphp? [y/N] " response
-case $response in
-		[yY][eE][sS]|[yY]) 
-	
+
+#-----------------------------------
+#   phpmyadmin default path change
+#-----------------------------------
+function Chang_phpMyAdmin() {
+echo -e "${YELLOW}Changing phpMyAdmin default path from /phpMyAdmin to /phpmyadmin...${NC}"
+
 cat >/etc/phpmyadmin/apache.conf <<EOL
 # phpMyAdmin default Apache configuration
 
-Alias /myadminphp /usr/share/phpmyadmin
+Alias /phpmyadmin /usr/share/phpmyadmin
 
 <Directory /usr/share/phpmyadmin>
 		Options FollowSymLinks
@@ -177,17 +203,25 @@ Alias /myadminphp /usr/share/phpmyadmin
 </Directory>
 EOL
 
-echo -e "${GREEN}Path was succesfully changed!
-New phpMyAdmin path is: /myadminphp (i.e.: yourwebsite.com/myadminphp)${NC}"
+echo -e "${GREEN}Path was succesfully changed/\nNew phpMyAdmin path is: /phpmyadmin (i.e.: yourwebsite.com/phpmyadmin)${NC}"
 
-				;;
-		*)
 
-	echo -e "${RED}Path was not changed!${NC}"
+}
 
-				;;
+#-----------------------------------
+
+read -r -p "Do you want to change default phpMyAdmin path to /phpMyAdmin? [y/N] " response
+echo -e "Default phpMyAdmin path to /phpMyAdmin? [y/N] " && response=y;
+
+case $response in
+		[yY][eE][sS]|[yY]) Chang_phpMyAdmin	;;
+		*) echo -e "${RED}Path was not changed!${NC}" ;;
 esac
 
+
+
+
+#-----------------------------------
 #creating user
 echo -e "${YELLOW}Adding separate user & creating website home folder for secure running of your website...${NC}"
 
@@ -205,16 +239,14 @@ echo -e "${YELLOW}Adding separate user & creating website home folder for secure
 	Website folder: /var/www/$domain${NC}"
 
 
-#configuring apache2
+
+
+#-----------------------------------
+#     configuring apache2
+#-----------------------------------
+function CONFIGURE_APACHE() {
 echo -e "${YELLOW}Now we going to configure apache2 for your domain name & website root folder...${NC}"
 
-read -r -p "Do you want to configure Apache2 automatically? [y/N] " response
-case $response in
-		[yY][eE][sS]|[yY]) 
-
-	echo -e "Please, provide us with your domain name: " && read domain
-	echo -e "Please, provide us with your email: "
-	read domain_email
 	cat >/etc/apache2/sites-available/$domain.conf <<EOL
 	<VirtualHost *:80>
 				ServerAdmin $domain_email
@@ -263,10 +295,19 @@ EOL
 		Your server public IP is: $P_IP (Please, set this IP into your domain name 'A' record)
 		Website was activated & apache2 service reloaded!
 		${NC}"
-				;;
-		*) echo -e "${RED}WARNING! Apache2 was not configured properly, you can do this manually or re run our script.${NC}" ;;
+
+}
+
+read -r -p "Do you want to CONFIGURE APACHE2 automatically? [y/N] " response
+case $response in
+	[yY][eE][sS]|[yY]) CONFIGURE_APACHE ;;
+	*) echo -e "\t\t\n${RED}WARNING! \nApache2 was not configured properly, you can do this manually.${NC}" ;;
 esac
 
+
+
+
+#-----------------------------------
 #downloading WordPress, unpacking, adding basic pack of plugins, creating .htaccess with optimal & secure configuration
 echo -e "${YELLOW}On this step we going to download latest version of WordPress with EN or RUS language, set optimal & secure configuration and add basic set of plugins...${NC}"
 
@@ -274,31 +315,23 @@ read -r -p "Do you want to install WordPress & automatically set optimal and sec
 case $response in
 	[yY][eE][sS]|[yY]) 
 
-echo -e -n "${GREEN}Please, choose WordPress language you need (set RUS or ENG): ${NC}
-\t 1. WordPress language English  [ENG]
-\t 2. WordPress language RUSSION  [RUS]
-\t 2. WordPress language DEFAULT  [ENG]
-\n\t 0. Cancel & Quite
-\nEnter: "
-
-
-## 
 while true; do
-  MENU
-  read rsn
-  case $rsn in
-    [1]* ) wordpress_lang='ENG';;
-    [2]* ) wordpress_lang='RUS';;
-    [2]* ) wordpress_lang='ENG';;
-    [0]* ) echo -e " ${YELLOW} Cancel and Break.. ${NC}"; break ;;
-  esac
+	echo -e -n "${GREEN}Please, choose WordPress language you need (set RUS or ENG): ${NC}\n\t 1. WordPress language English  [ENG]\n\t 2. WordPress language RUSSION  [RUS]\n\t 3. WordPress language DEFAULT  [ENG]\n\n CHOOSE: ";
+	read rsn
+	case $rsn in
+		[1]* ) wordpress_lang='ENG';echo -e "${YELLOW} WordPress language [English] ${NC}"; break;;
+		[2]* ) wordpress_lang='RUS';echo -e "${YELLOW} WordPress language [Russion] ${NC}"; break;;
+		[3]* ) wordpress_lang='ENG';echo -e "${YELLOW} Default WordPress language [English] ${NC}"; break;;
+		"" ) wordpress_lang='ENG'; echo -e "${YELLOW} WordPress language [English] ${NC}"; break;;
+		* ) wordpress_lang='ENG'; echo -e "${YELLOW}Default [ENG] ${NC}";;
+	esac
 done
 
 	if [ "$wordpress_lang" == 'RUS' ]; then
-		wget https://ru.wordpress.org/latest-ru_RU.zip -O /tmp/$wordpress_lang.zip
+		wget https://ru.wordpress.org/latest-ru_RU.zip -O /tmp/$wordpress_lang.zip;
 	elif [ "$wordpress_lang" == 'ENG' ]; then
-		wget https://wordpress.org/latest.zip -O /tmp/$wordpress_lang.zip
-	fi
+		wget https://wordpress.org/latest.zip -O /tmp/$wordpress_lang.zip;
+	fi;
 
 	echo -e "Unpacking WordPress into website home directory..."
 	sleep 5
@@ -309,27 +342,37 @@ done
 	mkdir /var/www/$domain/wp-content/uploads
 	chmod -R 777 /var/www/$domain/wp-content/uploads
 
-	echo -e "Now we going to download some useful plugins:
-	1. Google XML Sitemap generator
-	2. Social Networks Auto Poster
-	3. Add to Any
-	4. Easy Watermark"
-	sleep 7
+
+	echo -e "
+--------------------------------------------
+Now we going to download some useful plugins:
+--------------------------------------------
+\t 1. Google XML Sitemap generator
+\t 2. Social Networks Auto Poster
+\t 3. Add to Any
+\t 4. Easy Watermark
+--------------------------------------------
+";
+	sleep 3
 	
+	#------------
 	SITEMAP="`curl https://wordpress.org/plugins/google-sitemap-generator/ | grep https://downloads.wordpress.org/plugin/google-sitemap-generator.*.*.*.zip | awk '{print $3}' | sed -ne 's/.*\(http[^"]*.zip\).*/\1/p'`"
-	wget $SITEMAP -O /tmp/sitemap.zip
-	unzip /tmp/sitemap.zip -d /tmp/sitemap
+	wget $SITEMAP -O /tmp/sitemap.zip;
+	unzip /tmp/sitemap.zip -d /tmp/sitemap;
 	mv /tmp/sitemap/* /var/www/$domain/wp-content/plugins/
 
+	#------------
 	wget https://downloads.wordpress.org/plugin/social-networks-auto-poster-facebook-twitter-g.zip -O /tmp/snap.zip
 	unzip /tmp/snap.zip -d /tmp/snap
 	mv /tmp/snap/* /var/www/$domain/wp-content/plugins/
 
+	#------------
 	ADDTOANY="`curl https://wordpress.org/plugins/add-to-any/ | grep https://downloads.wordpress.org/plugin/add-to-any.*.*.zip | awk '{print $3}' | sed -ne 's/.*\(http[^"]*.zip\).*/\1/p'`"
 	wget $ADDTOANY -O /tmp/addtoany.zip
 	unzip /tmp/addtoany.zip -d /tmp/addtoany
 	mv /tmp/addtoany/* /var/www/$domain/wp-content/plugins/
 
+	#------------
 	WATERMARK="`curl https://wordpress.org/plugins/easy-watermark/ | grep https://downloads.wordpress.org/plugin/easy-watermark.*.*.*.zip | awk '{print $3}' | sed -ne 's/.*\(http[^"]*.zip\).*/\1/p'`"
 	wget $WATERMARK -O /tmp/watermark.zip
 	unzip /tmp/watermark.zip -d /tmp/watermark
@@ -337,17 +380,20 @@ done
 
 	rm /tmp/sitemap.zip /tmp/snap.zip /tmp/addtoany.zip /tmp/watermark.zip
 	rm -rf /tmp/sitemap/ /tmp/snap/ /tmp/addtoany/ /tmp/watermark/
-	
 	echo -e "Downloading of plugins finished! All plugins were transfered into /wp-content/plugins directory.${NC}" ;;
+	
 	*) echo -e "${RED}WordPress and plugins were not downloaded & installed. You can do this manually or re run this script.${NC}" ;;
 esac
 
-#creating of swap
-echo -e "On next step we going to create SWAP (it should be your RAM x2)..."
 
-read -r -p "Do you need SWAP? [y/N] " response
-case $response in
-		[yY][eE][sS]|[yY]) 
+
+
+#-----------------------------------
+#        CREATING OF SWAP
+#-----------------------------------
+
+function CREATING_SWAP() {
+	echo -e "On next step we going to create SWAP (it should be your RAM x2)..."
 
 	RAM="`free -m | grep Mem | awk '{print $2}'`"
 	swap_allowed=$(($RAM * 2))
@@ -355,17 +401,24 @@ case $response in
 	fallocate -l $swap /var/swap.img
 	chmod 600 /var/swap.img
 	mkswap /var/swap.img
-	swapon /var/swap.img
-
-	echo -e "${GREEN}RAM detected: $RAM
-	Swap was created: $swap${NC}"
+	swapon /var/swap.img;
+	echo -e "${GREEN}\n==========  RAM  ==========\nRAM detected:     $RAM\nSWAP was created: $swap${NC}"
 	sleep 5
+}
 
-				;;
-		*) echo -e "${RED}You didn't create any swap for faster system working. You can do this manually or re run this script.${NC}";;
+
+read -r -p "Do you need SWAP? [y/N] " response
+case $response in
+	[yY][eE][sS]|[yY]) CREATING_SWAP ;;
+	*) echo -e "${RED}Swap didn't create for system working.\nYou can do this manually or re run this script.${NC}";;
 esac
 
-#creation of secure .htaccess
+
+
+
+#-----------------------------------
+#   creation of secure .htaccess
+#-----------------------------------
 echo -e "${YELLOW}Creation of secure .htaccess file...${NC}"
 sleep 3
 cat >/var/www/$domain/.htaccess <<EOL
@@ -424,7 +477,12 @@ chmod 644 /var/www/$domain/.htaccess
 
 echo -e "${GREEN}.htaccess file was succesfully created!${NC}"
 
-#cration of robots.txt
+
+
+
+#-----------------------------------
+#      cration of robots.txt
+#-----------------------------------
 echo -e "${YELLOW}Creation of robots.txt file...${NC}"
 sleep 3
 cat >/var/www/$domain/robots.txt <<EOL
@@ -445,12 +503,13 @@ Disallow: /tag
 Disallow: /?author=*
 EOL
 
-echo -e "${GREEN}File robots.txt was succesfully created!
-Setting correct rights on user's home directory and 755 rights on robots.txt${NC}"
+echo -en "${GREEN}File robots.txt was succesfully created! \n Setting correct rights on user's home directory and 755 rights on robots.txt${NC}"
 sleep 3
-
 chmod 755 /var/www/$domain/robots.txt
 
+
+
+#-----------------------------------
 echo -e "${GREEN}Configuring fail2ban...${NC}"
 sleep 3
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.conf-old
@@ -503,12 +562,12 @@ EOL
 
 service fail2ban restart
 
-echo -e "${GREEN}fail2ban configuration finished!
-fail2ban service was restarted, default confige backuped at /etc/fail2ban/jail.conf-old
-Jails were set for: ssh bruteforce, ssh ddos, apache overflows${NC}"
-
+echo -e "${GREEN}fail2ban configuration finished! \n fail2ban service was restarted, default confige backuped at /etc/fail2ban/jail.conf-old \n Jails were set for: ssh bruteforce, ssh ddos, apache overflows${NC}"
 sleep 5
 
+
+
+#-----------------------------------
 echo -e "${GREEN} Configuring apache2 prefork & worker modules...${NC}"
 sleep 3
 cat >/etc/apache2/mods-available/mpm_prefork.conf <<EOL
@@ -534,38 +593,56 @@ cat > /etc/apache2/mods-available/mpm_worker.conf <<EOL
 EOL
 
 a2dismod status
-
-echo -e "${GREEN}Configuration of apache mods was succesfully finished!
-Restarting Apache & MySQL services...${NC}"
-
+echo -e "${GREEN}Configuration of apache mods was succesfully finished! \nRestarting Apache & MySQL services...${NC}"
 service apache2 restart
 service mysql restart
-
 echo -e "${GREEN}Services succesfully restarted!${NC}"
 sleep 3
 
 
-dbPassword=$(date +%s|sha256sum|base64|head -c 25) && db_pass="$dbPassword"
-wp_pass=$(date +%s|sha256sum|base64|head -c 20)
-dbNameandUser=$(echo ${domain} | tr "." "_" | tr "-" "_") && db_user="$dbNameandUser"
+echo -e "${GREEN}Adding user & database for WordPress, setting wp-config.php...${NC}"
+# echo -e "Please, set username for database: " && read db_user
+# echo -e "Please, set password for database user: " && read db_pass
+
+#mysql -u root -p <<EOF
+#CREATE USER '$db_user'@${Host} IDENTIFIED BY '$db_pass';
+#CREATE DATABASE IF NOT EXISTS $db_user;
+#GRANT ALL PRIVILEGES ON $db_user.* TO '$db_user'@${Host};
+#ALTER DATABASE $db_user CHARACTER SET utf8 COLLATE utf8_general_ci;
+#EOF
+################################
+echo "--Reusing credentials----------------------"
+$authorization -e "
+CREATE USER 'root'@${Host} IDENTIFIED BY '${db_pass}';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@${Host} WITH GRANT OPTION;
+DROP USER 'root'@'localhost';"
+
+$authorization -e "
+DROP USER IF EXISTS ${dbNameandUser}@${Host};
+DROP DATABASE IF EXISTS ${dbNameandUser};"
+
+$authorization -e "
+CREATE DATABASE ${dbNameandUser};
+CREATE USER ${dbNameandUser}@${Host} IDENTIFIED BY '${dbPassword}';
+GRANT ALL PRIVILEGES ON ${dbNameandUser}.* TO ${dbNameandUser}@${Host};
+FLUSH PRIVILEGES;"
+db_pass=${db_pass} >/dev/null 2>/dev/null
+db_pass=${db_pass} > /tmp/${db_pass}
 wp_admin=root
 wp_pass=$(date +%s|sha256sum|base64|head -c 20)
-Host=\'%\'
 
+apt-get install curl -y || apk add curl \
+&& curl -o /tmp/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+&& chmod +x /tmp/wp-cli.phar \
+&& mv /tmp/wp-cli.phar /usr/local/bin/wp \
+&& wp core download --path=/var/www/${domain} --locale=en_US --allow-root \
+&& wp config create --path=/var/www/${domain} --dbname=${dbNameandUser} --dbuser=${dbNameandUser} --dbpass=${dbPassword} --dbhost=localhost --allow-root --skip-check \
+&& wp core install --skip-email --url=${domain} --title=${domain} --admin_user=${wp_admin} --admin_password=${wp_pass} --admin_email=alina.m.giese@gmail.com --allow-root --path=/var/www/${domain}
 
-echo -e "${GREEN}Adding user & database for WordPress, setting wp-config.php...${NC}"
-echo -e "Please, set username for database: "
-# read db_user
-echo -e "Please, set password for database user: "
-#read db_pass
-
-mysql -u root -p <<EOF
-CREATE USER '$db_user'@${Host} IDENTIFIED BY '$db_pass';
-CREATE DATABASE IF NOT EXISTS $db_user;
-GRANT ALL PRIVILEGES ON $db_user.* TO '$db_user'@${Host};
-ALTER DATABASE $db_user CHARACTER SET utf8 COLLATE utf8_general_ci;
-EOF
-
+###mkdir /var/www/$domain/wp-content/uploads
+chmod 775 -R /var/www/$domain/ ###wp-content/uploads
+chown www-data:www-data -R /var/www/$domain
+#####################
 cat >/var/www/$domain/wp-config.php <<EOL
 <?php
 
@@ -624,27 +701,5 @@ ____________________________________________________
 Connect DB: mysql -h 127.0.0.1 -u root -p${db_pass}
 ====================================================
 " >> /tmp/credentials.txt
-
-echo "
-====================================================
-                    INFO
-====================================================
-Domainame:    ${domain}
-DATE:         $( date +%Y-%m-%d_%k%M%S )
-DBRootPass:   ${db_pass}
-DBName:       ${db_user}
-DBUser:       ${dbNameandUser}
-DBPass:       ${db_pass}
-WPAdmin:      ${wp_admin}
-WPPass:       ${wp_pass}
-____________________________________________________
-
-         Installation is complete!
-		 
-====================================================
-Connect DB: mysql -h 127.0.0.1 -u root -p${db_pass}
-====================================================
-"
-
-
+cat /tmp/credentials.txt
 echo -e "Installation & configuration succesfully finished."
